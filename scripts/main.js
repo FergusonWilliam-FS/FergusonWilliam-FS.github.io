f427cae13c69657d575f32d7d0d657787bd36c4e
WAFerguson1 <WAFerguson1@student.fullsail.edu>
2021-09-16 09:09:39 -0700

    Added js functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 var answer = prompt("What percentage of the speed of light can the Orion achieve?");
 
 while (answer <= 6 || answer >= 11) {
-    alert("That answer is not correct.");
+    alert("That answer is not correct. (Hint: more than 6, less than 10)");
     var answer = prompt("What percentage of the speed of light can the Orion achieve?");
 }
 
